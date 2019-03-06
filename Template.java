2206e2f5d43f508f3f07f81dec3840c08216a47f
Saad Bhuiyan <47544294+saadbhuiyan0@users.noreply.github.com>
2019-03-05 19:09:25 -0500

    Create Template.java

---
+++ Template.java
@@ -0,0 +1,27 @@
+/*********************************
+ * comments
+ *********************************/
+
+public class Template {
+
+	  //fields
+    String ;
+    int ;
+    double ;
+
+    public Template() {
+    }
+    
+    public Template(/*type arg*/) {
+    }
+
+    public String toString() {
+    }
+
+    public boolean equals(Template other) {
+    }
+
+    public static void main(String[] commandLine) {
+    }
+
+}
