649288e951ad383f650b987e2a6738865849ed2e
Saad Bhuiyan <47544294+saadbhuiyan0@users.noreply.github.com>
2019-03-05 19:11:34 -0500

    Create UserOfTemplate.java

---
+++ UserOfTemplate.java
@@ -0,0 +1,15 @@
+/*********************************
+ * comments
+ ********************************/
+
+public class UserOfTemplate {
+
+    public static /*type*/ oneTest(/*args*/) {
+		    return null;
+	  }
+	
+    public static void main(String[] commandLine) {
+		    System.out.println(oneTest(/*args*/));
+	  }
+
+}
