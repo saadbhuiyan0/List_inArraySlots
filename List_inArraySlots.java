7e45be37c401b093b36c4faf9da142c2de166a5b
Saad Bhuiyan <ssaad00@stuy.edu>
2019-03-06 18:19:58 -0500

    Change to skeletonHolmes template

--- List_inArraySlots.java
+++ List_inArraySlots.java
@@ -1,27 +1,54 @@
-/*********************************
- * comments
- *********************************/
+/**
+  Implement a list of integer elements, including
+  both data and operations.
+ */
 
 public class List_inArraySlots {
 
-    //fields
-    String ;
-    int ;
-    double ;
+    // declare fields here
 
+    /**
+      Construct an empty list with a small initial capacity.
+     */
     public List_inArraySlots() {
     }
+
+
+    /** 
+      @return the number of elements in this list
+     */
+    // public int size() {
+    // }
+
+
+     /** 
+       @return a string representation of this list,
+       in [a,b,c,] format
+      */ 
+    // public String toString() {
+    // }
+
     
-    public List_inArraySlots(/*type arg*/) {
-    }
+    /** 
+      Appends @value to the end of this list.
+      
+      @return true, in keeping with conventions yet to be discussed
+     */
+     // public boolean add( int value) {
+     // }
 
-    public String toString() {
-    }
 
-    public boolean equals(List_inArraySlots other) {
-    }
-
-    public static void main(String[] args) {
-    }
-
+    /** 
+      Double the capacity of the List_inArraySlots, 
+      preserving existing data
+     */
+     // private void expand() {
+        // System.out.println( "expand... (for debugging)");
+           // /* S.O.P. rules for debugging:
+              // Working methods should be silent. But during 
+              // development, the programmer must verify that 
+              // this method is called when that is appropriate.
+              // So test using the println(), then comment it out.
+              // */
+     // }
 }
