0d09cdac3f042770f8d1fa6f1509659cfb72fe57
Saad Bhuiyan <47544294+saadbhuiyan0@users.noreply.github.com>
2019-03-05 19:33:59 -0500

    Update UserOfList.java

--- UserOfList.java
+++ UserOfList.java
@@ -5,11 +5,10 @@
 public class UserOfList {
 
     public static /*type*/ oneTest(/*args*/) {
-		    return null;
-	  }
+    }
 	
     public static void main(String[] commandLine) {
-		    System.out.println(oneTest(/*args*/));
-	  }
+	System.out.println(oneTest(/*args*/));
+    }
 
 }
